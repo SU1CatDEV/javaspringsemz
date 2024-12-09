0f054b0e9fc8ae15c39f1604bb5a0c476b91ed78
SU1CatDEV <gergenkopzoya@gmail.com>
2024-12-10 01:56:55 +0700

    hw2

---
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,63 @@
+package com.example.demo.controllers;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public String findAll(Model model) {
+        List<User> users = userService.findAll();
+
+        model.addAttribute("users", users);
+        return "user-list";
+    }
+
+    @GetMapping("/user-create")
+    public String createUserForm(User user) {
+        return "user-create";
+    }
+
+    @PostMapping("/user-create")
+    public String createUser(User user) {
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/user-delete/{userId}")
+    public String deleteUser(@PathVariable(value="userId") Integer id, Model model) {
+        userService.deleteUser(id);
+        model.addAttribute("id", id);
+        return "user-delete";
+    }
+
+    @GetMapping("/user-update/{userId}")
+    public String updateUser(@PathVariable(value="userId") Integer id, Model model) {
+        User editing = userService.findById(id);
+        model.addAttribute("editing", editing);
+        return "user-update";
+    }
+
+    @PostMapping("/user-update")
+    public String updateUser(User user) {
+        userService.updateUser(user);
+        return "redirect:/users";
+    }
+
+}
