b17b15e66d81fb0c55a8fdd89b1b7f57db7db006
SU1CatDEV <gergenkopzoya@gmail.com>
2024-12-11 01:41:00 +0700

    free me from this flesh prison

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.example.demo.controllers;
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -49,7 +51,7 @@ public class UserController {
 
     @GetMapping("/user-update/{userId}")
     public String updateUser(@PathVariable(value="userId") Integer id, Model model) {
-        User editing = userService.findById(id);
+        User editing = userService.findById(id); // because im not about to reinvent the monstrosity which is backend prop drilling. no thats not a thing dont look it up.
         model.addAttribute("editing", editing);
         return "user-update";
     }
@@ -58,6 +60,15 @@ public class UserController {
     public String updateUser(User user) {
         userService.updateUser(user);
         return "redirect:/users";
+    }
+
+    @GetMapping("/user-json/{userId}")
+    public ResponseEntity getById(@PathVariable(value="userId") Integer id, Model model) {
+        User getted = userService.findById(id);
+        return new ResponseEntity(
+                getted,
+                HttpStatus.OK
+        );
     }
 
 }
